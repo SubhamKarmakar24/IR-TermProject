d49b70c9ec93b6953a2ad168184a1b473ce8ddf4
sankalp0104 <sankalp0104@hotmail.com>
2021-10-18 16:24:02 +0530

    Improved indexer and queries

--- PAT1_22_indexer.py
+++ PAT1_22_indexer.py
@@ -45,9 +45,10 @@ def main():
         tokens = word_tokenize(text_in_file)
         new_tokens = []
         for token in tokens:
-            modified_token = re.sub('[^a-zA-Z]', '', token)
-            if len(modified_token) > 1:
-                new_tokens.append(modified_token)
+            modified_tokens = re.sub('[^a-zA-Z]', ' ', token).split()
+            for modified_token in modified_tokens:
+                if len(modified_token) > 1:
+                    new_tokens.append(modified_token)
 
         stop_words = set(stopwords.words('english'))
         removed_stopwords = [
