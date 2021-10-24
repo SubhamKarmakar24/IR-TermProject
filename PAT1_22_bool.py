be48e0d1f021766d97ff3eb78d89c35e86f8fcec
Arnab Mallick <50038680+apprenticearnab@users.noreply.github.com>
2021-10-24 15:04:13 +0530

    Adding part 1C

---
+++ PAT1_22_bool.py
@@ -0,0 +1,46 @@
+import pickle
+import sys
+idx = {}
+pic_file = sys.argv[1]
+pickle_file = open(pic_file, "rb")
+idx = pickle.load(pickle_file)
+q_file = sys.argv[2]
+query_file = open(q_file, 'r')
+x = query_file.read()
+list_x = x.split('\n')
+query = []
+for i in list_x:
+    query.append([i.split(' ')[0],' '.join(i.split(' ')[1:])])
+
+def merge(arr1,arr2,n1,n2):
+    ans = []
+    i=0
+    j=0
+    while i<n1 and j<n2:
+        if arr1[i]==arr2[j]:
+            ans.append(arr1[i])
+            i+=1
+            j+=1
+        elif arr1[i]<arr2[j]:
+            i+=1
+        else:
+            j+=1
+    return ans
+bits = []
+for u in query[:-1]:
+    doc = {}
+    for f in u[1].split(' '):
+        if f in idx:
+            doc[len(idx[f])] = idx[f]
+    st = sorted(doc.keys())
+    if len(st)==len(u[1].split(' ')):
+        merged = doc[st[0]]
+        for i in range(1,len(st)):
+                merged = merge(merged,doc[st[i]],len(merged),len(doc[st[i]]))
+        
+    bits.append([u[0],[x[1].split('/')[-1] for x in merged]])  
+    
+file1 = open('PAT1_22_results.txt','a')
+for c in bits:
+    if len(c[1])!=0:
+        file1.write("{} : {}\n".format(c[0],', '.join(c[1])))
